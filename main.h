869b57275706ba774fa77a3c150f75f21c5af314
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-07 13:02:30 -0700

    Added funtion tokenizer

--- main.h
+++ main.h
@@ -1,19 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
-#define BUFFER 1024
+/**
+ * struct list_s - singly linked list
+ * @str: string tokenized of command line arguments.
+ * @next: points to the next node in the linked list.
+ *
+ * Description: singly linked list, the nodes are agregated at the end.
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_cmd;
 
-
-
-char *token_maker(char *input);
+list_cmd *add_node_at_end(list_cmd **head, const char *str);
+char **tokenizer(char *input);
+void free_list(list_cmd *head);
 
 #endif /* MAIN_H */
