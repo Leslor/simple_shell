17e19946b54fefbb6aadc156288d21212d1f3b7b
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 15:37:09 -0700

    Fixed with Betty Style

--- tokens.c
+++ tokens.c
@@ -3,29 +3,30 @@
 /**
  * tokenizer - - function that create tokens
  * @input: is a char pointer of a string
- * @delim: is a char pointer delimitator
- * 
+ * @delimt: is a char pointer delimitator
+ *
  * Return: double pointer to strings
  */
 
 char **tokenizer(char *input, char *delimt)
 {
-        int count = 0;
-        char **av = NULL;
+	int count = 0;
+	char **av = NULL;
 
-        printf("Empezando a tokenizer\n");
-        av = malloc(sizeof(char *) * 1024);
+	printf("Empezando a tokenizer\n");
+	av = malloc(sizeof(char *) * 1024);
 
-        if (!av)
-                {  free(input);
-                    return(NULL);
-                }
-        while ((av[count] = strtok(input, delimt)) != NULL)
-        {
-                input = NULL;
-                printf("Este es el av[%d]: %s\n ", count, av[count]);
-                count++;
-        }
-        printf("Finalizando tokenizer\n");
-        return (av);
+	if (!av)
+	{
+		free(input);
+		return (NULL);
+	}
+	while ((av[count] = strtok(input, delimt)) != NULL)
+	{
+		input = NULL;
+		printf("Este es el av[%d]: %s\n ", count, av[count]);
+		count++;
+	}
+	printf("Finalizando tokenizer\n");
+	return (av);
 }
