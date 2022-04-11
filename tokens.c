ea2175967c55dff18aac5828cca6bf1768433b9a
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 13:18:43 -0700

    Fixed with Betty style

--- tokens.c
+++ tokens.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * tokenizer - - function that create tokens
+ * @input: is a char pointer of a string
+ * @delim: is a char pointer delimitator
+ * 
+ * Return: double pointer to strings
+ */
+
 char **tokenizer(char *input, char *delimt)
 {
         int count = 0;
