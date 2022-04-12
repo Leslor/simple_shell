4080a89c672363676a509d5c90a46b4f1dcbd8af
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 22:30:31 -0700

    Fixed Betty Style

--- tokens.c
+++ tokens.c
@@ -13,7 +13,6 @@ char **tokenizer(char *input, char *delimt)
 	int count = 0;
 	char **av = NULL;
 
-	printf("Empezando a tokenizer\n");
 	av = malloc(sizeof(char *) * 1024);
 
 	if (!av)
@@ -24,9 +23,7 @@ char **tokenizer(char *input, char *delimt)
 	while ((av[count] = strtok(input, delimt)) != NULL)
 	{
 		input = NULL;
-		printf("Este es el av[%d]: %s\n ", count, av[count]);
 		count++;
 	}
-	printf("Finalizando tokenizer\n");
 	return (av);
 }
