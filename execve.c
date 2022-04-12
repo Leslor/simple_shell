4001e93590a034f3698e646cb6bbcf826cb5cdb6
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 17:17:53 -0700

    Fixed with Betty Style

--- execve.c
+++ execve.c
@@ -1,35 +1,39 @@
 #include "main.h"
 
-/*Excuve */
+/**
+ * _execve - execute commands
+ * @argVec: argument vecto of input
+ *
+ * Return: void
+ */
+
 void _execve(char **argVec)
 {
-        pid_t child_pid;
-        int status;
-        extern char **environ;
+	pid_t child_pid;
+	int status;
 
-        printf("Starting the execution Process\n");
+	printf("Starting the execution Process\n");
+	child_pid = fork();
 
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-                perror("Error: ");
-                exit(0);
-        }
-        else if (child_pid == 0)
-        {
-                printf("=========================================\n");
-                if (execve(argVec[0], argVec, environ) == -1)
-                {
-                        perror(argVec[0]);
-                        exit(0);
-                }
-                /*SIGKILl: para terminar el proceso*/
-                kill(child_pid, SIGKILL);
-        }
-        else if (child_pid > 0)
-        {
-                wait(&status);
-        }
-        printf("Finish the execution Process\n");
+	if (child_pid == -1)
+	{
+		perror("Error: ");
+		exit(0);
+	}
+	else if (child_pid == 0)
+	{
+		printf("=========================================\n");
+		if (execve(argVec[0], argVec, environ) == -1)
+		{
+			perror(argVec[0]);
+			exit(0);
+		}
+		/*SIGKILl: para terminar el proceso*/
+		kill(child_pid, SIGKILL);
+	}
+	else if (child_pid > 0)
+	{
+		wait(&status);
+	}
+	printf("Finish the execution Process\n");
 }
