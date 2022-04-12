442f5b8a1cbe7dd26b76fcd23f20803f1c3265c2
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 17:25:27 -0700

    Fixed Betty Style

--- _access.c
+++ _access.c
@@ -1,15 +1,21 @@
 #include"main.h"
 
+/**
+ * _access - evaluates if a *av exist in the env
+ * @av: argument vector
+ *
+ * Return: rt_value 0 if the file exist, or -1 otherwise.
+ */
+
 int  _access(char *av)
 {
+	int rt_value;
 
-int rt_value;
+	rt_value = access(av, X_OK);
+	if (rt_value == 0)
+		printf("File: %s can execute rt_value = %d\n", av, rt_value);
+	else
+		printf("File: %s can't execute rt_value= %d\n", av, rt_value);
 
-   rt_value=access(av,X_OK);
-   if(rt_value==0)
-         printf("File:%s can execute   rt_value=%d\n",av,rt_value);
-    else
-           printf("File:%s can't execute  rt_value=%d \n",av,rt_value);
-
-  return (rt_value);
+	return (rt_value);
 }
