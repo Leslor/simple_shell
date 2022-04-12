6fea35935003c10757faab875aa8aaa24b034c64
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-11 22:02:03 -0700

    Fixed Betty Style

--- fress.c
+++ fress.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
-/*
-NULL(string de caracteres en nulo) = "\0\0\0\0";
-nulo(caracter nulo) = '\0';
-*/
+/**
+ * _free - frees double pointer
+ * @av: input double pointer
+ *
+ * Return: void
+ */
 
 void _free(char **av)
 {
-        int i = 0;
-        if (*(av + i)) /* *(av + 1) == av[1] */
-        {
-                free(*(av + i));
-        }
-        free(av);
+	int i = 0;
+
+	if (*(av + i))
+	{
+		free(*(av + i));
+	}
+	free(av);
 }
